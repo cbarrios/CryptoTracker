d3fa1f84691ce6a3379877314a921d62b153f414
Carlos Barrios <lalobarrios16@yahoo.com>
2025-05-15 00:46:35 -0400

    (1) Coin List Screen UI

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/ui/models/CoinUi.kt
@@ -0,0 +1,55 @@
+package com.plcoding.cryptotracker.crypto.ui.models
+
+import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hr: DisplayableChange,
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
+
+data class DisplayableChange(
+    val number: DisplayableNumber,
+    val isNegative: Boolean
+)
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        changePercent24Hr = DisplayableChange(
+            number = changePercent24Hr.toDisplayableNumber(),
+            isNegative = changePercent24Hr < 0.0
+        ),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
