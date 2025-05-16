1a361daad9bb5737289434c152aa5c777f85d032
Carlos Barrios <lalobarrios16@yahoo.com>
2025-05-15 20:01:49 -0400

    (2) Network Layer Utility

--- app/src/main/java/com/plcoding/cryptotracker/crypto/ui/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/ui/models/CoinUi.kt
@@ -2,7 +2,7 @@ package com.plcoding.cryptotracker.crypto.ui.models
 
 import androidx.annotation.DrawableRes
 import com.plcoding.cryptotracker.crypto.domain.Coin
-import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import com.plcoding.cryptotracker.core.ui.util.getDrawableIdForCoin
 import java.text.NumberFormat
 import java.util.Locale
 
