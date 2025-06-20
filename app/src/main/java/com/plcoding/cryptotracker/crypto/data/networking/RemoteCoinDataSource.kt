60d8ec73b795514620748c04ec1bc9814d9deb82
Carlos Barrios <lalobarrios16@yahoo.com>
2025-06-20 19:24:33 -0400

    (8) 2 - Loading Coin Details

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,26 +6,70 @@ import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
 import com.plcoding.cryptotracker.core.domain.util.map
 import com.plcoding.cryptotracker.crypto.data.mappers.toCoin
+import com.plcoding.cryptotracker.crypto.data.mappers.toCoinPrice
+import com.plcoding.cryptotracker.crypto.data.networking.dto.CoinHistoryDto
 import com.plcoding.cryptotracker.crypto.data.networking.dto.CoinsResponseDto
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
 ) : CoinDataSource {
 
+    private companion object {
+        private const val API_KEY =
+            "a1b20e43cac386b11f5fcbff28e7f0651b31c48f7267046367f7f4d9d6fe2e1e"
+    }
+
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
         return safeCall<CoinsResponseDto> {
             httpClient.get(
-                urlString = constructUrl("/assets?apiKey=a1b20e43cac386b11f5fcbff28e7f0651b31c48f7267046367f7f4d9d6fe2e1e")
-            )
+                urlString = constructUrl("/assets")
+            ) {
+                parameter("apiKey", API_KEY)
+            }
         }.map { response ->
             withContext(Dispatchers.Default) {
                 response.data.map { it.toCoin() }
+            }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        val endMillis = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        return safeCall<CoinHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("apiKey", API_KEY)
+                parameter("interval", "h6")
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            withContext(Dispatchers.Default) {
+                response.data.map { coinPriceDto ->
+                    coinPriceDto.toCoinPrice()
+                }
             }
         }
     }
--- app/src/main/java/com/plcoding/cryptotracker/crypto/ui/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/ui/coin_list/CoinListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.ui.models.CoinUi
 import com.plcoding.cryptotracker.crypto.ui.models.toCoinUi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
@@ -16,6 +17,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.time.ZonedDateTime
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
@@ -36,10 +38,28 @@ class CoinListViewModel(
     fun onAction(action: CoinListAction) {
         when (action) {
             is CoinListAction.OnCoinClick -> {
-                _uiState.update {
-                    it.copy(selectedCoin = action.coinUi)
-                }
+                selectCoin(action.coinUi)
             }
+        }
+    }
+
+    private fun selectCoin(coinUi: CoinUi) {
+        _uiState.update {
+            it.copy(selectedCoin = coinUi)
+        }
+        viewModelScope.launch {
+            coinDataSource
+                .getCoinHistory(
+                    coinId = coinUi.id,
+                    start = ZonedDateTime.now().minusDays(5),
+                    end = ZonedDateTime.now()
+                )
+                .onSuccess { history ->
+                    println(history)
+                }
+                .onError { error ->
+                    _events.send(CoinListEvent.Error(error))
+                }
         }
     }
 
