cf22c3736ea2c4e321154a95973991226b0bf08d
Carlos Barrios <lalobarrios16@yahoo.com>
2025-06-23 21:49:08 -0400

    (9) 3 - Building the Custom Line Chart

--- app/src/main/java/com/plcoding/cryptotracker/crypto/ui/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/ui/coin_list/CoinListViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
+import com.plcoding.cryptotracker.crypto.ui.coin_detail.DataPoint
 import com.plcoding.cryptotracker.crypto.ui.models.CoinUi
 import com.plcoding.cryptotracker.crypto.ui.models.toCoinUi
 import kotlinx.coroutines.Dispatchers
@@ -18,6 +20,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
@@ -54,12 +57,37 @@ class CoinListViewModel(
                     start = ZonedDateTime.now().minusDays(5),
                     end = ZonedDateTime.now()
                 )
-                .onSuccess { history ->
-                    println(history)
+                .onSuccess { prices ->
+                    loadHistory(prices)
                 }
                 .onError { error ->
                     _events.send(CoinListEvent.Error(error))
                 }
+        }
+    }
+
+    private fun loadHistory(prices: List<CoinPrice>) {
+        viewModelScope.launch {
+            val dataPoints = withContext(Dispatchers.Default) {
+                prices
+                    .sortedBy { it.dateTime }
+                    .map {
+                        DataPoint(
+                            x = it.dateTime.hour.toFloat(),
+                            y = it.priceUsd.toFloat(),
+                            xLabel = DateTimeFormatter
+                                .ofPattern("ha\nM/d")
+                                .format(it.dateTime)
+                        )
+                    }
+            }
+            _uiState.update {
+                it.copy(
+                    selectedCoin = it.selectedCoin?.copy(
+                        coinPriceHistory = dataPoints
+                    )
+                )
+            }
         }
     }
 
--- app/src/main/java/com/plcoding/cryptotracker/crypto/ui/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/ui/models/CoinUi.kt
@@ -3,6 +3,7 @@ package com.plcoding.cryptotracker.crypto.ui.models
 import androidx.annotation.DrawableRes
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.core.ui.util.getDrawableIdForCoin
+import com.plcoding.cryptotracker.crypto.ui.coin_detail.DataPoint
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -14,7 +15,8 @@ data class CoinUi(
     val marketCapUsd: DisplayableNumber,
     val priceUsd: DisplayableNumber,
     val changePercent24Hr: DisplayableChange,
-    @DrawableRes val iconRes: Int
+    @DrawableRes val iconRes: Int,
+    val coinPriceHistory: List<DataPoint> = emptyList()
 )
 
 data class DisplayableNumber(
